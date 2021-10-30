529190ab6009829b546278e7a67c024a3c2216e0
Albus Dompeldorius <albus.dompeldorius@gmail.com>
2021-10-30 20:43:13 +0200

    Flake lock

--- flake.nix
+++ flake.nix
@@ -1,28 +1,3 @@
-#{
-#  description = "Minimal flake project";
-#
-#  inputs.flake-utils = {
-#    url = "github:numtide/flake-utils";
-#    inputs.nixpkgs.follows = "nixpkgs";
-#  };
-#
-#  outputs =
-#    { self
-#    , nixpkgs
-#    , flake-utils
-#    }:
-#    flake-utils.lib.eachDefaultSystem
-#      (system:
-#      rec {
-#        packages = flake-utils.lib.flattenTree {
-#          hello = nixpkgs.legacyPackages.${system}.hello;
-#        };
-#        # defaultPackage = packages.hello;
-#        # apps.hello = flake-utils.lib.mkApp { drv = packages.hello; };
-#        # defaultApp = apps.hello;
-#
-#      });
-#}
 {
   description = "Springrollup";
 
